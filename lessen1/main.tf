ad31cf28cc83de3b2d516fa58cca70699273d89d
roota5666 <109865537+roota5666@users.noreply.github.com>
2023-10-29 12:27:48 +0900

    add lesson1

---
+++ lessen1/main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg_example"
+  location = "japaneast"
+  tags = {
+    owner = "example@example.com"
+    source = "terraform"
+  }
+}
